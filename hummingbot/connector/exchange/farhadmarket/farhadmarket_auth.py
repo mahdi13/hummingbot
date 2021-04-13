aa6519e8f0e720da167017d882fb7214523fb7fb
Mahdi Aali <mahdi13.1373@gmail.com>
2021-04-13 13:39:34 +0300

    Add farhadmarket auth

---
+++ hummingbot/connector/exchange/farhadmarket/farhadmarket_auth.py
@@ -0,0 +1,21 @@
+from typing import Dict, Any
+
+
+class FarhadmarketAuth():
+    """
+    Auth class required by FarhadMarket API
+    """
+    def __init__(self, api_key: str, secret_key: str):
+        self.api_key = api_key
+        self.secret_key = secret_key
+
+    def get_headers(self) -> Dict[str, Any]:
+        """
+        Generates authentication headers required by farhadmarket
+        :return: a dictionary of auth headers
+        """
+        return {
+            "X-API-KEY": self.api_key,
+            "X-API-SECRET": self.secret_key,
+            "Content-Type": "application/x-www-form-urlencoded",
+        }

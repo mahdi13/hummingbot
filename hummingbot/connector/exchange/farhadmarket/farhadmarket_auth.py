415b95ab1222d245934b1158c23bc0e11c95f5a2
Mahdi Aali <mahdi13.1373@gmail.com>
2021-04-14 21:05:44 +0300

    Fix minor bugs of farhadmarket

--- hummingbot/connector/exchange/farhadmarket/farhadmarket_auth.py
+++ hummingbot/connector/exchange/farhadmarket/farhadmarket_auth.py
@@ -5,6 +5,7 @@ class FarhadmarketAuth():
     """
     Auth class required by FarhadMarket API
     """
+
     def __init__(self, api_key: str, secret_key: str):
         self.api_key = api_key
         self.secret_key = secret_key
--- hummingbot/connector/exchange/farhadmarket/farhadmarket_utils.py
+++ hummingbot/connector/exchange/farhadmarket/farhadmarket_utils.py
@@ -1,4 +1,6 @@
-from hummingbot.core.utils.tracking_nonce import get_tracking_nonce
+import math
+
+from hummingbot.core.utils.tracking_nonce import get_tracking_nonce, get_tracking_nonce_low_res
 
 from hummingbot.client.config.config_var import ConfigVar
 from hummingbot.client.config.config_methods import using_exchange
@@ -8,6 +10,11 @@ CENTRALIZED = True
 EXAMPLE_PAIR = "ETH-USDT"
 
 DEFAULT_FEES = [0.2, 0.4]
+
+
+# convert milliseconds timestamp to seconds
+def ms_timestamp_to_s(ms: int) -> int:
+    return math.floor(ms / 1e3)
 
 
 # Request ID class
@@ -20,6 +27,11 @@ class RequestId:
     @classmethod
     def generate_request_id(cls) -> int:
         return get_tracking_nonce()
+
+
+# get timestamp in milliseconds
+def get_ms_timestamp() -> int:
+    return get_tracking_nonce_low_res()
 
 
 def convert_from_exchange_trading_pair(exchange_trading_pair: str) -> str:
